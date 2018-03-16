d615753d00ef904def0d97e4df23084b6f4b3296
gxs746 <gxs746@cs.bham.ac.uk>
2018-03-16 17:20:28 +0000

    added cancellation prediction

--- pc/src/controller/Main.java
+++ pc/src/controller/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.log4j.Logger;
 
 import communication.Connection;
+import main.java.CancellationPrediction.MakePredictions;
 import main.java.JobSelection.*;
 
 public class Main {
@@ -12,6 +13,8 @@ public class Main {
 	private static final Logger logger = Logger.getLogger(Run.class);
 	
 	public static void main(String[] args) {
+		MakePredictions.main();
+		logger.debug("All predictions made.");
 		Run.main();
 		ArrayList<Order> orders =Run.getOrders();
 		logger.debug("Starting connection between pc and robot");
