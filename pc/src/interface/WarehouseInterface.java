505f4d769df71fcdc5316ab6d800b4a4f86be5b4
Unknown <will.tetlow@gmail.com>
2018-03-17 20:03:17 +0000

    add basic functionality to warehouse interface

--- pc/src/interface/WarehouseInterface.java
+++ pc/src/interface/WarehouseInterface.java
@@ -20,87 +20,87 @@ import java.awt.FlowLayout;
 
 public class WarehouseInterface extends JFrame implements ActionListener {
 
-  private static String[] jobArray;
-  private JList<String> jobList;
+    private static List<String> jobArray = new LinkedList<>();
+    private JList<String> jobList;
+    private static List<String> cancelArray = new LinkedList<>();
+    private int index;
+    private DefaultListModel<String> listModel;
 
-  public static void main(String[] args) {
-    jobReader();
-    SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        new WarehouseInterface();
-      }
-    });
-  }
+    public static void main(String[] args) {
+        jobReader();
+        SwingUtilities.invokeLater(WarehouseInterface::new);
+    }
 
-  public static void jobReader() {
-    String filePath = "../resources/jobs.csv";
-    BufferedReader reader = null;
-    String line = "";
-    String csvSplitBy = "\\r?\\n";
+    private static void jobReader() {
+        String filePath = "C:/Users/Will/warehouse-assignment/pc/resources/jobs.csv";
+        BufferedReader reader = null;
+        String line;
+        String csvSplitBy = "\\r?\\n";
 
-    try {
-      reader = new BufferedReader(new FileReader(filePath));
-      while ((line = reader.readLine()) != null) {
-        jobArray = line.split(csvSplitBy);
-
-        for(int i = 0; i < jobArray.length; i++) {
-          System.out.println(jobArray[i]);
-        }
-      }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (reader != null) {
         try {
-          reader.close();
+            reader = new BufferedReader(new FileReader(filePath));
+            while ((line = reader.readLine()) != null) {
+                jobArray.add(Arrays.toString(line.split(csvSplitBy)));
+
+//                for (String aJobArray : jobArray) {
+//                    System.out.println(aJobArray);
+//                }
+                System.out.println(jobArray.size());
+            }
         } catch (IOException e) {
-          e.printStackTrace();
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-      }
-    }
-  }
-
-  public WarehouseInterface() {
-    this.getContentPane().setLayout(new FlowLayout());
-
-    DefaultListModel<String> listModel = new DefaultListModel<>();
-    for (int i = 0; i < jobArray.length; i++) {
-      listModel.addElement(jobArray[i]);
     }
 
-    jobList = new JList<>(listModel);
-    jobList.addListSelectionListener(new ListSelectionListener() {
-      @Override
-      public void valueChanged(ListSelectionEvent e) {
-        if (!e.getValueIsAdjusting()) {
-          final List<String> selectedValuesList = jobList.getSelectedValuesList();
-          System.out.println(selectedValuesList);
+    private WarehouseInterface() {
+        this.getContentPane().setLayout(new FlowLayout());
+
+        listModel = new DefaultListModel<>();
+        for (int i = 0; i < jobArray.size(); i++) {
+            listModel.add(i, jobArray.get(i).substring(1, 6));
         }
-      }
-    });
 
-    add(new JScrollPane(jobList));
+        jobList = new JList<>(listModel);
+        jobList.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                final List<String> selectedValuesList = jobList.getSelectedValuesList();
+                index = jobList.getSelectedIndex();
+                cancelArray.add(0, jobList.getSelectedValue());
+                // System.out.println(selectedValuesList);
+                System.out.println(cancelArray.get(0));
+            }
+        });
 
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setTitle("Warehouse Interface");
-    this.setSize(200, 200);
-    this.setLocationRelativeTo(null);
-    this.setVisible(true);
+        add(new JScrollPane(jobList));
 
-    JButton b1 = new JButton("Cancel");
-    add(b1);
-    b1.addActionListener(this);
-    b1.setActionCommand("cancel");
-  }
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Warehouse Interface");
+        this.setSize(200, 225);
+        this.setLocationRelativeTo(null);
+        this.setResizable(true);
+        this.setVisible(true);
+        jobList.setFixedCellWidth(100);
 
-  public void actionPerformed(ActionEvent e) {
-    String action = e.getActionCommand();
-    if (action.equals("cancel")) {
-      System.out.println("cancel");
+        JButton b1 = new JButton("Cancel");
+        add(b1);
+        b1.addActionListener(this);
+        b1.setActionCommand("cancel");
     }
-  }
+
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if (action.equals("cancel")) {
+            listModel.remove(index);
+            // System.out.println(cancelArray.get(0));
+        }
+    }
 
 }
