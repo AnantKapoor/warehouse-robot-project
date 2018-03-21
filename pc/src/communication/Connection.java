f292aeec348a33936a37d3a73870f829f5e5389e
HXV740 <hxv740@cs.bham.ac.uk>
2018-03-21 18:54:57 +0000

    adding map and boolean checks to see that all robots have received paths before executing any of the routes (the writeInt(50) is designedto be a code to tell the robot to execute the routes

--- pc/src/communication/Connection.java
+++ pc/src/communication/Connection.java
@@ -4,6 +4,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.apache.log4j.Logger;
@@ -21,9 +24,11 @@ public class Connection implements Runnable {
 	private static final Logger logger = Logger.getLogger(Run.class);
 	
 	public static ArrayList<Order> allOrders;
+	private static String[] robotNames = {"OptimusPrime", "BumbleBee", "IronHide"};
     private DataInputStream m_dis;
     private DataOutputStream m_dos;
     private final NXTInfo m_nxt;
+    private static Map<String, Boolean> robotPathFinished = new LinkedHashMap<String, Boolean>();
 
     public Connection(NXTInfo _nxt) {
         m_nxt = _nxt;
@@ -44,12 +49,15 @@ public class Connection implements Runnable {
 
     @Override
     public void run() {
+
     	int rank;
 	        try {
 	        	if(isConnected()) {
 	        		logger.debug("PC connected to a robot" + m_nxt.name);
 	        		System.out.println("connected to " + m_nxt.name);
+	        		
 	        	}
+	        	readReply();
 	            while (isConnected()) {
 	            	
 	            	for (Order ord : allOrders) {
@@ -72,26 +80,29 @@ public class Connection implements Runnable {
 	            			}
 	            			
 	            			rank = i%3;
-	            			System.out.print("rank " + rank + " ");
 	            			
-	            			switch(rank) {
-	            			case 0:
-	            				checkRobot(steps, "IronHide");
-	            				break;
-	            			case 1:
-	            				checkRobot(steps, "BumbleBee");
-	            				break;
-	            			case 2:
-	            				checkRobot(steps, "OptimusPrime");
-	            				break;
-	            		
-	            			}
-	         
-	        				
 
+                            switch(rank) {
+                                case 0:
+                                    checkRobot(steps, robotNames[2]);
+                                    System.out.println(rank + " " + m_nxt.name);
+                                    break;
+                                case 1:
+                                    checkRobot(steps, robotNames[1]);
+                                    System.out.println(rank + " " + m_nxt.name);
+                                    break;
+                                case 2:
+                                    checkRobot(steps, robotNames[0]);
+                                    System.out.println(rank + " " + m_nxt.name);
+                                    break;
+                            }
+                            if(robotPathFinished.get(robotNames[0])&&robotPathFinished.get(robotNames[1])&&robotPathFinished.get(robotNames[2])){
+                                m_dos.writeInt(50);
+                                m_dos.flush();
+                                robotPathFinished.put(m_nxt.name, false);
+                            }
 	            		}
 	            	}
-	                readReply();
 	            }
 
 	        } catch (IOException e) {
@@ -117,25 +128,30 @@ public class Connection implements Runnable {
 	}
 
 	private void readReply() throws IOException {
-		int answer = m_dis.readInt();
+        System.out.println("Waiting for reply from " + m_nxt.name);
+        int answer = m_dis.readInt();
 		System.out.println(m_nxt.name + " returned " + answer);
+        robotPathFinished.put(m_nxt.name, true);
 	}
 
 
     /**
-     * @param args
+     * @param orders
      */
     public static void main(ArrayList<Order> orders) {
     	allOrders = orders;
+        for(int i = 0; i <3; i++){
+            robotPathFinished.put(robotNames[i], false);
+        }
         try {
         	
             NXTInfo[] nxts = {
 
-                    new NXTInfo(NXTCommFactory.BLUETOOTH, "OptimusPrime", "00:16:53:0A:97:1B"),
+                    new NXTInfo(NXTCommFactory.BLUETOOTH, robotNames[0], "00:16:53:0A:97:1B"),
 
-                   	new NXTInfo(NXTCommFactory.BLUETOOTH, "BumbleBee", "00:16:53:15:5F:A3"),
+                   	new NXTInfo(NXTCommFactory.BLUETOOTH, robotNames[1], "00:16:53:15:5F:A3"),
                    	
-                   	new NXTInfo(NXTCommFactory.BLUETOOTH, "IronHide", "00:16:53:0A:9A:AB"),
+                   	new NXTInfo(NXTCommFactory.BLUETOOTH, robotNames[2], "00:16:53:0A:9A:AB"),
                    	
                    	/*new NXTInfo(NXTCommFactory.BLUETOOTH, "Megatron", "00:16:53:08:9B:0D"),*/};
             
